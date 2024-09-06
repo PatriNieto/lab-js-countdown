520d70a5bbd7a6e411a79025bd89d71a227bb502
PatriNieto <patricianieto2.0@gmail.com>
2024-09-06 08:58:38 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -8,12 +8,38 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
+const startButtonNode = document.querySelector("#start-btn");
 
+startButtonNode.addEventListener("click", ()=>{
+  startCountdown()
+})
 
 
 // ITERATION 2: Start Countdown
+//div#timeNode
+const timeNode = document.querySelector("#time");
+
 function startCountdown() {
   console.log("startCountdown called!");
+  
+  timer = setInterval (()=>{
+    
+    if(remainingTime === 0){
+      console.log("entr called!");
+  
+      //the countdown timer should be stopped  
+      clearInterval(timer);
+      startButtonNode.disabled = true
+      showToast();
+    } else {
+      remainingTime--;
+      timeNode.innerText = remainingTime
+    }
+    
+
+  },1000 );
+
+  
 
 
   // Your code goes here ...
@@ -21,18 +47,26 @@ function startCountdown() {
 
 
 
-
+const toastNode = document.querySelector("#toast");
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
 
+  toastNode.classList.add("show");
 
+  setTimeout(()=>{
+    toastNode.classList.remove("show");
+  },3000)
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+  let closeButtonNode = document.querySelector("#close-toast")
+  .addEventListener("click", ()=>{
+    toastNode.classList.remove("show");
+  })
 
 }
